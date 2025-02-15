9bec8128e4775e5bc8ca88dd8f8223a5ec90dd31
AhmadSaleh2001 <ahmadmfsaleh@gmail.com>
2025-02-15 21:33:47 +0200

    add readme

--- Linux Memory Manager/main.c
+++ Linux Memory Manager/main.c
@@ -27,7 +27,7 @@ void print_employee_info(emp_t * emp){
 int main() {
 
     init_mmap();
-    // MM_REGISTER_STRUCT(std_t);
+    MM_REGISTER_STRUCT(std_t);
     MM_REGISTER_STRUCT(emp_t);
     mm_print_registered_page_families();
 
